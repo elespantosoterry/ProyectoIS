cc70b8085a2b56c6a678173c92c9e646743bc71f
unknown <brandon.valencia@ucsp.edu.pe>
2019-11-29 15:03:44 -0330

    Clase examen

--- Sistema de Gestión de Cursos y Exámenes/Sistema de Gestión de Cursos y Exámenes/generacionExamenes.cs
+++ Sistema de Gestión de Cursos y Exámenes/Sistema de Gestión de Cursos y Exámenes/generacionExamenes.cs
@@ -10,11 +10,40 @@ using System.Windows.Forms;
 
 namespace Sistema_de_Gestión_de_Cursos_y_Exámenes
 {
+    struct Examen
+    {
+        List<int> Preguntas; //Indices de pregunta
+        string Nombre;
+        string Tipo; //Control parcial final
+        int Tiempo;
+        int Npreguntas; //cantidad
+        List<Grupo> Grupos;
+        List<int> Puntajes;
+        bool CambiarPregunta(int a, int b)//cambio de indices indice del examen con indice del banco
+        {
+            //Preguntas[a] = [b];
+            return true;
+        }
+        bool CambiarPuntaje(int a, int b)
+        {
+            return true;
+        }
+        void mostrarExamen()
+        {
+
+        }
+
+    }
     public partial class generacionExamenes : Form
     {
         public generacionExamenes()
         {
             InitializeComponent();
+        }
+
+        private void generacionExamenes_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
